e5e7fef9d6b21ea0d368982d44ce0031b0a75f09
Kostya Borisov <borisov.kostya999@gmail.com>
2025-06-12 10:28:16 +0300

    Cadr colors

--- app/src/main/java/com/example/vk/CardVk.kt
+++ app/src/main/java/com/example/vk/CardVk.kt
@@ -1,21 +1,21 @@
 package com.example.vk
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material.icons.rounded.MoreVert
-import androidx.compose.material.icons.rounded.Send
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -23,22 +23,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-@Preview
+
 @Composable
-fun Card(){
-    Column {
-        PostHeader()
-        Text(stringResource(R.string.ipsum),Modifier.padding(8.dp), color = MaterialTheme.colorScheme.onPrimary)
-        Image(painter = painterResource(R.drawable.kod), contentDescription = "", modifier = Modifier.fillMaxWidth())
-        Spacer(Modifier.height(12.dp))
-        Statistics()
+fun CardVk(modifier: Modifier){
+    Card (modifier = modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background
+        )){
+        Column {
+            Spacer(Modifier.height(12.dp))
+            PostHeader()
+            Text(stringResource(R.string.ipsum),Modifier.padding(8.dp), color = MaterialTheme.colorScheme.onPrimary)
+            Image(painter = painterResource(R.drawable.kod), contentDescription = "", modifier = Modifier.fillMaxWidth())
+            Spacer(Modifier.height(12.dp))
+            Statistics()
+            Spacer(Modifier.height(8.dp))
+        }
     }
+
 }
 
 
